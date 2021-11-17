b1f674b206ba96cf60cb1f5b9ffced42f62110e9
Imran Rather <emmeiwhite@gmail.com>
2021-11-17 21:54:28 +0530

    reduce() [playground of acc, currItem]

--- importantHOFs/map_filter.js
+++ importantHOFs/map_filter.js
@@ -1,9 +1,9 @@
 // 1. map() : Returns an Array
 
 const people = [
-  { name: "Emmei", age: 29, designation: "Developer" },
-  { name: "Roufi", age: 30, designation: "Teacher" },
-  { name: "Rafia", age: 28, designation: "FMS Engineer" },
+  { id: 1, name: "Emmei", age: 29, salary: 300, designation: "Developer" },
+  { id: 2, name: "Roufi", age: 30, salary: 200, designation: "Teacher" },
+  { id: 3, name: "Rafia", age: 28, salary: 500, designation: "FMS Engineer" },
 ];
 
 console.log(people.map((person) => person.age));
@@ -28,3 +28,20 @@ console.log(filteredPersons);
 
 const filteredPerson = people.find((person) => person.age <= 29);
 console.log(filteredPerson);
+
+// 4. reduce() : Iterates, callback function
+// Reduces to single value - number, array, object
+// First Parameter ('acc') - total of all calculations
+// Second Parameter ('curr') - current iteration/value
+
+// Let's find the total salary for the people array
+
+console.log("Time to play with reduce() function");
+const totalSalary = people.reduce(function (acc, currItem) {
+  console.log(`Current salary is ${currItem.salary}`);
+  acc += currItem.salary;
+  console.log(`accumlates to ${acc}`);
+  return acc; // We must note that always the accumulator is returned
+}, 0); // Since, Initial value of acc is number, we must return number in acc
+
+console.log(`Total salary is ${totalSalary}`);
