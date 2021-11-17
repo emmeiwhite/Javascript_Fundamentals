0e3f19d83e0571cd77ef821305ac0e2435b2fb87
Imran Rather <emmeiwhite@gmail.com>
2021-11-17 14:00:23 +0530

    HOF, Callbacks and Closures - Definitions

---
+++ HOF_CBs/hof.js
@@ -0,0 +1,22 @@
+/* --- 1. Function Expressions | First Class Citizens --- */
+
+/* 
+   Functions in Javascript are call First Class Objects / Citizens.
+   - Stored in a variable (expression)
+   - Passed as an argument to another funtion
+   - Returned from the function (closure)
+*/
+
+/* --- 2. Higher Order Function --- */
+
+/* 
+   A function which accepts another function as an arugument or
+   returns another function as a result is called HOF
+*/
+
+/* --- 3. Call Back Function --- */
+
+/* 
+   A function which is passed to another function as an arugument and 
+   executed inside that function is a Callback function
+*/
