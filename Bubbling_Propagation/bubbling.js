3812fb8c732b10285edea1241577047b52da256d
Imran Rather <emmeiwhite@gmail.com>
2021-11-24 12:01:30 +0530

    Basic Definitions of Event Propation, Bubbling

---
+++ Bubbling_Propagation/bubbling.js
@@ -0,0 +1,4 @@
+// Allows us to select dynamic elements
+// Event Propagation - order in which the events are fired
+// Event Bubbling - event is executed on the clicked element first then bubbles up -- default
+// Event Capturing - fires at the root and fires until it reaches the target
