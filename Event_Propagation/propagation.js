968f33f110d3b1bbe774a458813ce3ecd3b4541d
Imran Rather <emmeiwhite@gmail.com>
2021-11-24 17:07:46 +0530

    Events not being applied on Dynamically generated elements

--- Event_Propagation/propagation.js
+++ Event_Propagation/propagation.js
@@ -1,6 +1,18 @@
-let heading = document.querySelector(".heading");
+// let heading = document.querySelector(".heading");
 let button = document.querySelector(".btn");
+let container = document.querySelector(".container");
 
+// Let's click on the button and creating the heading element on the run time
+// And try to assign the event handler again to the h2
+
+button.addEventListener("click", (e) => {
+  const heading = document.createElement("h2");
+  heading.classList.add("heading");
+  heading.textContent = "I am heading 2";
+  container.appendChild(heading);
+});
+
+let heading = document.querySelector(".heading");
 heading.addEventListener("click", (e) => {
   console.log("heading CLICKED");
 });
